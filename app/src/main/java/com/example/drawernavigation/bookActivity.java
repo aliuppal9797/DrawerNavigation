260e560c3423be4ae11aa63a2f605814d419c36d
Hafiz Muhammad Ali <80312742+aliuppal9797@users.noreply.github.com>
2021-06-08 19:13:52 +0500

    header section and menu section is added in drawer navigation

---
+++ app/src/main/java/com/example/drawernavigation/bookActivity.java
@@ -0,0 +1,14 @@
+package com.example.drawernavigation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class bookActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_book);
+    }
+}
