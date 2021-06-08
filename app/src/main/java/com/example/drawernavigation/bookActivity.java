7c3a7f3dd49e19688b7193d6b34cfb7e6a9f3269
Hafiz Muhammad Ali <80312742+aliuppal9797@users.noreply.github.com>
2021-06-09 00:19:29 +0500

    drawer navigatio is added

--- app/src/main/java/com/example/drawernavigation/bookActivity.java
+++ app/src/main/java/com/example/drawernavigation/bookActivity.java
@@ -3,6 +3,7 @@ package com.example.drawernavigation;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class bookActivity extends AppCompatActivity {
 
@@ -10,5 +11,7 @@ public class bookActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_book);
+        Toast.makeText(getApplicationContext(),"Return is Clicked",Toast.LENGTH_LONG).show();
+
     }
 }
